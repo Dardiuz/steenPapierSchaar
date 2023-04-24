0bb26e666ecd04056de3b4cf43223190f2e256f1
Dardiuz <donnynoorlander@gmail.com>
2023-04-24 12:24:36 +0200

    added eventlistener button function

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 let playerScore = 0;
 let computerScore = 0;
 
+const rockButton = document.querySelector(".rock");
+const paperButton = document.querySelector(".paper");
+const scissorsButton = document.querySelector(".scissors");
+const resultDiv = document.querySelector(".results");
+
 function computerChoice() {
   const choices = ["rock", "paper", "scissors"];
   return choices[Math.floor(Math.random() * choices.length)];
@@ -22,19 +27,28 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-function game() {
+rockButton.addEventListener("click", () => {
   const computerSelection = computerChoice();
+  const playerSelection = "rock";
   playRound(playerSelection, computerSelection);
   console.log(playerSelection, computerSelection);
-}
-if (playerScore > computerScore) {
+  console.log(playRound(playerSelection, computerSelection));
   console.log(playerScore, computerScore);
-  return "you won from the computer";
-} else if (playerScore === computerScore) {
-  return "You tied!";
-} else {
-  return "You lost from the computer";
-}
+});
 
-game();
-console.log(game());
+// function game() {
+//   const computerSelection = computerChoice();
+//   playRound(playerSelection, computerSelection);
+//   console.log(playerSelection, computerSelection);
+// }
+// if (playerScore > computerScore) {
+//   console.log(playerScore, computerScore);
+//   return "you won from the computer";
+// } else if (playerScore === computerScore) {
+//   return "You tied!";
+// } else {
+//   return "You lost from the computer";
+// // }
+
+// game();
+// console.log(game());
